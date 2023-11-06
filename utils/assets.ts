c02077bcf8ec80fc6866895d4774f6cebd910ad8
viandwi24 <viandwi24@pm.me>
2023-11-06 13:29:23 +0700

    feat: add support to deploy app engine

--- utils/assets.ts
+++ utils/assets.ts
@@ -1,4 +1,4 @@
 export const getPublicUrl = (path: string) => {
   const config = useRuntimeConfig()
-  return `${config?.public?.publicUrl || ''}${path}`;
+  return `${config?.public?.publicUrl || ''}${path}?ignoreCache=1&r=${Math.random()}`;
 }
